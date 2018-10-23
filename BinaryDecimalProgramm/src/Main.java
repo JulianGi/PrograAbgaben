5f2c4e2bf3a76054af1391dec53f78577b6ddbe3
JulianGi <julian.giesen@outlook.com>
2018-10-23 17:55:21 +0200

    Decimal Teil fertig

--- BinaryDecimalProgramm/src/Main.java
+++ BinaryDecimalProgramm/src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
     public static void main(String[] args) {
         String mode = SimpleIO.getString("Bitte  waehlen  Sie aus:\n" +
@@ -7,8 +6,73 @@ public class Main {
 
         if (mode.equals("Decimal"))
         {
-            int decimalWert = SimpleIO.getInt("Bitte geben Sie eine ganze Zahl zwischen -128 und 127 ein.");
+            int decimalWert = 128;
+            String binaryString = ""; 
+            
 
+            while (!(-128 < decimalWert && decimalWert < 127))
+            {
+                decimalWert = SimpleIO.getInt("Bitte geben Sie eine ganze Zahl zwischen -128 und 127 ein.");
+            }
+            boolean negativ = decimalWert < 0;
+
+            while(decimalWert != 0)
+            {
+                if(decimalWert % 2 == 0)
+                {
+                    binaryString = "0" + binaryString;
+                }
+                else
+                {
+                    binaryString = "1" + binaryString;
+                }
+                decimalWert /= 2;
+            }
+            while(binaryString.length() != 8)
+            {
+                binaryString = "0" + binaryString;
+            }
+            
+
+            if(negativ)
+            {
+                //Flippen der bits
+                String flippedString = "";
+                for(int i = 0; i < binaryString.length(); i++)
+                {
+                    if(binaryString.charAt(i) == '1')
+                    {
+                        flippedString = flippedString + '0';
+                    }
+                    else
+                    {
+                        flippedString = flippedString + '1';
+                    }
+                }
+
+                //Eins addieren
+                String plusString = "";
+                for(int i = flippedString.length() - 1; i > 0; i--)
+                {
+                    if(flippedString.charAt(i) == '1')
+                    {
+                        plusString ="0" + plusString ;
+                    }
+                    else
+                    {
+                        plusString = "1" + plusString ;
+                        for(int x = i - 1; x >= 0; x--)
+                        {
+                            plusString = flippedString.charAt(x) + plusString;
+                        }
+                        break;
+                    }
+                }
+                binaryString = plusString;
+            }
+            SimpleIO.output(binaryString, "Success");
+
+            
         }
 
         else if (mode.equals("Binary"))
