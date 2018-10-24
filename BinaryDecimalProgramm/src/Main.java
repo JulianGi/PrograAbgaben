8c14ebd2c5e7e42321e5f7ee701268e202bbdd33
Levin <lev.gaeher@gmail.com>
2018-10-24 13:30:10 +0200

    Fixed

--- BinaryDecimalProgramm/src/Main.java
+++ BinaryDecimalProgramm/src/Main.java
@@ -9,56 +9,30 @@ public class Main {
                 "Decimal: Dezimalzahl  ins  Zweierkomplement  konvertieren.\n" +
                 "Binary: 8-Bit -Zweierkomplement  als  Dezimalzahl  darstellen.");
         }
+        
         if (mode.equals("Decimal"))
         {
             int decimalWert = 128;
-            String binaryString = ""; 
-            
-
             while (!(-128 <= decimalWert && decimalWert <= 127))
             {
                 decimalWert = SimpleIO.getInt("Bitte geben Sie eine ganze Zahl zwischen -128 und 127 ein.");
             }
-            boolean negativ = decimalWert < 0;
-
-            while(decimalWert != 0)
-            {
-                if(decimalWert % 2 == 0)
-                {
-                    binaryString = "0" + binaryString;
-                }
-                else
-                {
-                    binaryString = "1" + binaryString;
-                }
-                decimalWert /= 2;
-            }
-            while(binaryString.length() != 8)
-            {
-                binaryString = "0" + binaryString;
-            }
             
-
-            if(negativ)
-            {
-                binaryString = zweierKomplement(binaryString);
-            }
-            SimpleIO.output(binaryString, "Success");
-
-            
+            String binaryString = DecToBin(decimalWert);
+            SimpleIO.output(binaryString, "Success");            
         }
 
         else if (mode.equals("Binary"))
         {
             String binaryWert = "0";
-            int decimalWert = 0;
-            //Längen Check
+
+            // Laengen Check
             while (binaryWert.length() != 8)
             {
                 binaryWert = SimpleIO.getString("Bitte geben Sie eine ganze Zahl im 8-Bit -Zweierkomplement ein.");
             }
 
-            //Binär Check
+            // Binaer Check
             for(int i = 0; i < binaryWert.length(); i++)
             {
                 if(binaryWert.charAt(i) != '0' && binaryWert.charAt(i) != '1')
@@ -69,37 +43,13 @@ public class Main {
                 }
             }
             
-
-            boolean negativ = binaryWert.charAt(0) == '1';
-            if(negativ)
-            {
-                binaryWert = zweierKomplement(binaryWert);
-            }
-
-            int counter = binaryWert.length();
-            for(int i = 0; i < binaryWert.length(); i++)
-            {
-                counter--;
-                if(binaryWert.charAt(i) == '1')
-                {
-                    decimalWert += pow(2,counter);
-                }
-                
-            }
-            
-            if(negativ)
-            {
-                decimalWert *= -1;
-            }
-            
-            String result = "" + decimalWert;
-
-            SimpleIO.output(result,"Success");
-
+            int decimalWert = BinToDec(binaryWert);
+            SimpleIO.output(decimalWert + "", "Success");
         }
         
 
     }
+    
     public static int pow(int basis, int exponent)
     {
         if(exponent == 0)
@@ -114,79 +64,107 @@ public class Main {
         }
         return temp;
     }
+    
 
-    public static String zweierKomplement(String bString)
+    public static int BinToDec (String binaryWert)
     {
-        //Flippen der bits
-        String flippedString = "";
-        for(int i = 0; i < bString.length(); i++)
+    	int decimalWert = 0;
+    	
+    	boolean negativ = binaryWert.charAt(0) == '1';
+    	
+    	if(negativ)
+    	{
+    		String normBinary = removeSign(binaryWert);
+
+            // Without sign, abs of 128 is possible
+            // First sign is always only set for -128
+    		
+    		normBinary = zweierKomplement(binaryWert);
+
+            System.out.println("BinToDec:" + binaryWert + " got turned into sec Komp. " + normBinary + " of length " + normBinary.length());
+            binaryWert = normBinary;
+    	}
+        
+        for(int i = 0; i < binaryWert.length(); i++)
         {
-            if(bString.charAt(i) == '1')
-            {
-                flippedString = flippedString + '0';
-            }
-            else
-            {
-                flippedString = flippedString + '1';
+            if(binaryWert.charAt(i) == '1')
+                decimalWert += pow(2, binaryWert.length()-i-1);
+        }
+        
+        if(negativ)
+            decimalWert *= -1;
+        
+        System.out.println("BinToDec:" + binaryWert + " got converted into decimal " + decimalWert + "");
+        
+        
+        return decimalWert;
+    }
+
+    
+    public static String DecToBin (int decimalWert)
+    {
+    	boolean negativ = decimalWert < 0;
+        if (negativ) 
+        	decimalWert = -decimalWert;
+    	
+    	String binaryString = "";
+    	
+    	int bits = 8;
+        for (int i = bits-1; i >= 0; i--)
+        {
+            binaryString = binaryString + ((decimalWert / pow(2, i)) % 2);
+        }
+        
+        // We now have an unsigned int, without sign but allows for (-)128
+
+        System.out.println("DecToBin:" + decimalWert + " got turned into " + binaryString + " of length " + binaryString.length());
+        
+        if(negativ)
+        {
+        	binaryString = zweierKomplement(binaryString);
+        	// We now have a 2er Komplement, still without sign
+            
+            binaryString = addSign(binaryString);
+            // Now with proper sign, since -128 has been packed to 7 bits
+            
+            System.out.println("DecToBin:" + decimalWert + " got turned into sec Komp. " + binaryString + " of length " + binaryString.length());   
+        }
+         	
+        return binaryString;
+    }
+
+    public static String zweierKomplement(String binaryString)
+    {
+    	// Sign-Bit wird gel�scht, um -128 m�glich zu machen
+    	// Invertiere erst alle Bits
+    	// Addiere 1: Alle Bits bis zur ersten 0 von rechts werden NOCHMAL invertiert
+    	
+        String secKomplement = "";
+        boolean added = false;
+        for(int i = binaryString.length() - 1; i >= 0; i--)
+        {
+        	boolean one = binaryString.charAt(i) == '0'; // Flip
+        	if(one) // 1
+        	{ // Continue until we can add
+        		secKomplement = (added? "1" : "0") + secKomplement;
+        	}
+            else // 0
+            { // Add 1 if not yet added
+            	secKomplement = (added? "0" : "1") + secKomplement;
+            	added = true;
             }
         }
-
-        //Eins addieren
-        return(binärAddieren(flippedString, "00000001"));
         
+        return secKomplement;
     
     }
-    public static String binärAddieren(String a, String b)
-    {   
-        
-        String result = "";
-        boolean carry = false;
-        for(int i = a.length() - 1; i>=0; i--)
-        {
-            int x = a.charAt(i);
-            int y = b.charAt(i);
-            if(x+y == 98)
-            {
-                if (carry)
-                {
-                    result = "1" + result;
-                }
-                else
-                {
-                    result = "0" + result;
-                    carry = true;
-                }
-                
-                
-            }
-            else if(x+y == 97)
-            {
-                if (carry)
-                {
-                    result = "0" +result;
-                }
-                else
-                {
-                    result = "1" +result;
-                }
-            }
-            else if(x+y == 96)
-            {
-                if (carry)
-                {
-                    result = "1" +result;
-                    carry = false;
-                }
-                else
-                {
-                    result = "0" +result;
-                }
-            }
-        }
-
-
-
-        return result;
+    
+    public static String removeSign (String bin)
+    {
+    	return "0" + bin.substring(1);
     }
-   
+    public static String addSign (String bin)
+    {
+    	return "1" + bin.substring(1);
+    }
 }
