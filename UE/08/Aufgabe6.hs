94a8a712468e684f7be1000f66a5d8a6e801cb69
JulianGi <julian.giesen@outlook.com>
2018-12-19 17:17:35 +0100

    Nr 6 fertig

--- UE/08/Aufgabe6.hs
+++ UE/08/Aufgabe6.hs
@@ -24,3 +24,21 @@ isDiv a b   | modulo a b == 0   = True
 sumUp :: [Int] -> Int
 sumUp []    = 0
 sumUp (x:xs) = x + sumUp xs
+
+
+multLists :: [Int] -> [Int] -> [Int]
+multLists (x:[]) (y:[]) = [x*y]
+multLists (x:xs) (y:ys) = (x*y) : multLists xs ys
+multLists _ _ = []
+
+
+binRep :: Int -> (Int, [Int])
+binRep n    | n == 0    = (0, [0])
+            | n > 1     = (1, binRepHelper n)
+            | n < 1     = (-1, binRepHelper ((-1)*n))
+
+
+binRepHelper :: Int -> [Int]
+binRepHelper n  | n == 0 = []
+                | otherwise = binRepHelper (div n 2) ++ [modulo n 2]
+
