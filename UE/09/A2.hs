f0c1b1edf3193285cb3132b2ed2f4be32163ff31
Levin G <lev.gaeher@gmail.com>
2019-01-08 17:42:52 +0100

    NR 6

--- UE/09/A2.hs
+++ UE/09/A2.hs
@@ -1,27 +1,28 @@
 
--- Types
+-- a)
 data VariableName = X | Y deriving Show
-data Expression = Constant Int | Variable VariableName | Add Expression Expression |  Multiply Expression Expression deriving Show
 
--- Resolves Variables
 getValue :: VariableName -> Int
 getValue X = 5
 getValue Y = 13
 
--- Resolves Expressions
+-- b)
+data Expression = Constant Int | Variable VariableName | Add Expression Expression |  Multiply Expression Expression deriving Show
+
+-- c)
 evaluate :: Expression -> Int
 evaluate (Constant c) = c
 evaluate (Variable v) = getValue v
 evaluate (Add ex1 ex2) = (evaluate ex1) + (evaluate ex2)
 evaluate (Multiply ex1 ex2) = (evaluate ex1) * (evaluate ex2)
 
--- Optimizes Expressions One Level
+-- d)
 tryOptimize :: Expression -> Expression
 tryOptimize (Add (Constant c1) (Constant c2)) = Constant (c1 + c2)
 tryOptimize (Multiply (Constant c1) (Constant c2)) = Constant (c1 * c2)
 tryOptimize ex = ex
 
--- Optimizes Expressions Recursively
+-- e)
 evaluatePartially :: Expression -> Expression
 evaluatePartially (Add ex1 ex2) = tryOptimize (Add (evaluatePartially ex1) (evaluatePartially ex2))
 evaluatePartially (Multiply ex1 ex2) = tryOptimize (Multiply (evaluatePartially ex1) (evaluatePartially ex2))
---
+++ UE/09/A6.hs
@@ -0,0 +1,43 @@
+
+-- a)
+data Optional a = Empty | Present a deriving Show
+
+mapOptional :: (a -> b) -> Optional a -> Optional b
+mapOptional _ Empty = Empty
+mapOptional f (Present o) = Present (f o)
+
+-- b)
+filterOptional :: (a -> Bool) -> Optional a -> Optional a
+filterOptional f Empty = Empty
+filterOptional f (Present o) = if f o then Present o else Empty 
+
+-- c)
+foldOptional :: (a -> b) -> b -> Optional a -> b
+foldOptional _ x Empty = x
+foldOptional f _ (Present o) = f o
+
+-- d)
+data Product = Article String Int deriving Show
+
+isHumanEatable :: Product -> Bool
+isHumanEatable (Article "Dog Food" _) = False
+isHumanEatable (Article "Pizza" _) = True 
+
+adjustPrice :: Product -> Product
+adjustPrice (Article n p) = if p < 1000 then Article n (p*2) else Article n p
+
+stringify :: Product -> String
+stringify (Article n p) = "The Article named '" ++ n ++ "' costs " ++ show(p) ++ " Cents"
+
+-- e)
+filterHumanEatable :: Product -> Optional Product
+filterHumanEatable a = filterOptional isHumanEatable (Present a)
+
+adjustPriceO :: Optional Product -> Optional Product
+adjustPriceO a = mapOptional adjustPrice a
+
+stringifyO :: Optional Product -> String
+stringifyO a = foldOptional stringify "This article is unavailable" a
+
+toPriceTag :: Product -> String
+toPriceTag a = stringifyO (adjustPriceO (filterHumanEatable a))
