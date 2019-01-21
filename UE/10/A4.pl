87875c338e95f9398b700431f48ce07a4c2d16ae
JulianGi <julian.giesen@outlook.com>
2019-01-21 17:01:04 +0100

    Blatt 10 A4e)

---
+++ UE/10/A4.pl
@@ -0,0 +1,19 @@
+%a)
+isList(nil).
+isList(cons(_, Rest)) :- isList(Rest).
+
+%b)
+toPrologList(nil, []).
+toPrologList(cons(N, Rest),[Head|Tail]) :- Head = N, toPrologList(Rest,Tail). 
+
+%c)
+flatten([],[]).
+flatten([[]|Res], YS) :- flatten(Res, YS).
+flatten([[X|XS]|Res], [X|YS]) :- flatten([XS|Res], YS).
+
+%d)
+appendElement(X,Y,Z) :- flatten([X,[Y]],Z).
+
+%e)
+
+
